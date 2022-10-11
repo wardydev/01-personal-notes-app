ce06c6455b9220c46a881ba46e181b3939e8cefb
wardy30 <hairul.wardy@digitalent.id>
2022-10-12 06:32:30 +0800

    repair dark mode component function

--- src/components/SearchBar/index.js
+++ src/components/SearchBar/index.js
@@ -14,7 +14,9 @@ const SearchBar = () => {
       <input
         type="text"
         className={`form-control form-control-lg ${
-          getThemeStatus() === "true" ? "bg-dark" : "bg-light"
+          getThemeStatus() === "true"
+            ? "bg-dark text-light"
+            : "bg-light text-dark"
         }`}
         placeholder="Cari Catatan"
         aria-label="Cari Catatan"
